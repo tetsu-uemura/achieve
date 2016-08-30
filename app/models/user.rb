95fe638ade2a2dd728b84bdf3ef46cf08cd2f6b7
tetsu_uemura2 <tetsu1126uemura.com>
2016-08-30 04:01:56 +0000

    9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :blogs
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-         has_many :blogs
+
 end
