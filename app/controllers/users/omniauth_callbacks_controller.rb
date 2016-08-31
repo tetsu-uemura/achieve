b9f3d106fc8bd005c79a69d348f42924b33d21c3
tetsu_uemura2 <tetsu1126uemura.com>
2016-08-31 02:06:05 +0000

    14

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     # You need to implement the method below in your model
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
-
+    binding.pry
     if @user.persisted?
       set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
       sign_in_and_redirect @user, event: :authentication
@@ -23,5 +23,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-  
+
 end
